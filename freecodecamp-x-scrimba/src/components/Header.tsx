4c3a1000faf5f9be53cf9df5f29e66ceda9a99f7
Ken <kennethjay0022@gmail.com>
2025-10-18 22:46:36 +0800

    goat header changes

--- freecodecamp-x-scrimba/src/components/Header.tsx
+++ freecodecamp-x-scrimba/src/components/Header.tsx
@@ -1,22 +1,32 @@
 import logo from "../assets/logo.png"
 import logoName from "../assets/logo-name.png"
+import headerVideo from '../assets/header.mp4'
 
 const Header = () => {
   return (
+    <>
     <header className="header-content">
-        <div className="logo-container">
-            <img src={logo} className="w-15"/>
-            <img src={logoName} className="w-55"/>
-        </div>
-        <nav className="nav-content">
-            <ul>
-                <a href="/"> <li>About</li> </a>
-                <a href="/"> <li>Home</li> </a>
-                <a href="/"> <li>Projects</li> </a>
-            </ul>
-        </nav>
+      <video autoPlay loop muted className="bg-video" >
+        <source src={headerVideo} type="video/mp4"/>
+      </video>
+      <div className="overlay"></div>
+
+      <div className="logo-container">
+        <img src={logo} className="w-15"/>
+        <img src={logoName} className="w-55"/>
+
+      </div>
+      <nav className="nav-content">
+        <ul>
+          <a href="/"> <li>About</li> </a>
+          <a href="/"> <li>Home</li> </a>
+          <a href="/"> <li>Projects</li> </a>
+        </ul>
+      </nav>
         
     </header>
+    
+    </>
   )
 }
 
